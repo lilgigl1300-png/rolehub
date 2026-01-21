22ac20ebf0ba60b89f9ededf32a94a56cbcf4ffa
lilgigl1300-png <lilgigl1300@gmail.com>
2026-01-22 00:39:36 +0300

    Update MASTERS_CSV_URL to use worker URL

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const SHEET_CSV_URL = "https://long-sound-b57c.lilgigl1300.workers.dev/games.csv
 
 // CSV мастеров (через Cloudflare Worker)
 // Лист в Google Sheets должен называться: masters
-const MASTERS_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSlpiViZzoUWVgwxBgdqSjBniPPLsjPRZ8MGM4tg-xApL839KSAS7oCn8yOuqXj--FI1akhYjLAKgQy/pubhtml?gid=1565056969&single=true";
+const MASTERS_CSV_URL = "https://<твой-worker>.workers.dev/masters.csv";
 
 // Endpoint для отправки заявок
 const SUBMIT_API_URL = "https://long-sound-b57c.lilgigl1300.workers.dev";
