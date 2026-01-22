ae775669ce375185010584477bed2c215048f23f
lilgigl1300-png <lilgigl1300@gmail.com>
2026-01-22 21:43:14 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // === НАСТРОЙКИ (поменяй под себя) ===
 const SHEET_CSV_URL = "https://long-sound-b57c.lilgigl1300.workers.dev/games.csv";
-const MASTERS_CSV_URL = "https://long-sound-b57c.lilgigl1300.workers.dev/masters.csv";
+const MASTERS_CSV_URL = "";
 const SUBMIT_API_URL = "https://long-sound-b57c.lilgigl1300.workers.dev";
 const TG_USERNAME = "iwwitich";
